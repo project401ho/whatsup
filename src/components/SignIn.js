ba6fba75399fd6b126aeedffbabaf6b6f2433919
YuSung Lee <project401ho@gmail.com>
2021-06-17 08:53:20 +0900

    6.17
    build test

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -3,12 +3,12 @@ import {Auth} from 'aws-amplify';
 import {Button, TextField } from "@material-ui/core";
 import {useHistory} from 'react-router-dom'
 
-const SignIn = ({onSignIn}) => {
+const SignIn = ({onSignIn, onSignUp}) => {
 
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const history = useHistory()
-
+  
   const signIn = async () => {
     try{
       const user = await Auth.signIn(username, password)
@@ -16,6 +16,13 @@ const SignIn = ({onSignIn}) => {
       onSignIn(user)
     } catch(error){
       console.log("error in signing in", error);
+    }
+  }
+  const signUp = async () => {
+    try{
+      const user = await Auth.signUp(username, password)
+    }catch (error){
+      console.log("error in signing up", error);
     }
   }
 
@@ -39,8 +46,11 @@ const SignIn = ({onSignIn}) => {
       />
       </div>
       <div>
-      <Button id="isgnInButton" color="primary" onClick ={signIn}>
-        signIn
+      <Button id="signInButton" color="primary" onClick ={signIn}>
+        로그인
+      </Button>
+      <Button id="signUpButton" color="primary" onClick ={signIn}>
+        가입하기
       </Button>
       </div>
     </div>
