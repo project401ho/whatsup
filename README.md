8e0184ce6e31e8c08187663e1ecce00931ce5b69
YuSung Lee <project401ho@gmail.com>
2021-06-17 11:08:10 +0900

    6.17

--- README.md
+++ README.md
@@ -10,9 +10,6 @@ https://master.d2y0wpanbqeoy2.amplifyapp.com/
 익명 게시판<br>
 공지<br>
 게시물 조회수 보이기<br>
-게시물 수 만큼 페이지 계산 및 보이기<br>
-current page에 따라 불러오는 post 다르게 하기<br>
-테이블 항목 수 가져와서 리스트 no 제대로 뿌려주기<br>
 네비게이션 CSS<br>
 
 
@@ -21,12 +18,28 @@ Post css 수정<br>
 게시물 좋아요 기능<br>
 댓글 좋아요 기능<br>
 
+## Pagination
 
-## 기타:
-가입하기 버튼 <br>
-
+current page에 따라 불러오는 post 다르게 하기<br>
+nextToken Pagination 시 포스트 fetch가 순서대로 안됌<br>
+예상 workflow:
+1. 포스트 테이블 길이 확보 (이걸로 페이지 버튼 갯수 정하기)
+2. 테이블 항목 수 가져와서 리스트 no 제대로 뿌려주기
+3. 포스트 날짜별로 정렬 한뒤 limit 만큼 가져오기
+4. 숫자 버튼 클릭시 fetchContentList() 호출 (nextToken으로 페이지네이션 됨)
+5. current_page 하나 늘리기
+6. 다음/이전 버튼 클릭시 current_page 알맞게 갱신 및 fetch 또 호출
 
 # 업데이트 기록
+## 6.17
+가입 하기 버튼 및 기능 구현 <br>
+이메일 자동 컨펌<br>
+이메일 중복 체크 불가 (유저풀을 다시 만들어야 함)<br>
+없는 유저 입력시 알림창<br>
+가입시 바로 로그인 진행<br>
+있는 유저이름으로 가입 시도시 알림창 및 에러 띄우기<br>
+
+
 ## 6.16
 GraphQL createAt 별로 소팅 하는 다큐먼트 <br>
 https://docs.amplify.aws/guides/api-graphql/query-with-sorting/q/platform/js#implementation <br>
--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -3,10 +3,11 @@ import {Auth} from 'aws-amplify';
 import {Button, TextField } from "@material-ui/core";
 import {useHistory} from 'react-router-dom'
 
-const SignIn = ({onSignIn, onSignUp}) => {
+const SignIn = ({onSignIn}) => {
 
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [email, setEmail] = useState('')
   const history = useHistory()
   
   const signIn = async () => {
@@ -15,12 +16,27 @@ const SignIn = ({onSignIn, onSignUp}) => {
       history.push('/')
       onSignIn(user)
     } catch(error){
-      console.log("error in signing in", error);
+      if(error.code === "UserNotFoundException"){
+        alert("등록되지 않은 유저 입니다.")
+      }
+      console.log("error in signing in 123", error);
     }
   }
   const signUp = async () => {
     try{
-      const user = await Auth.signUp(username, password)
+      await Auth.signUp(username, password, email)
+        .then(()=>{
+          signIn()
+        })
+        .catch((error)=>{
+          if(error['code'] === "UsernameExistsException"){ 
+            alert("이미 등록된 유저입니다.")
+          }
+          else{
+            console.log(error);
+          }
+        })
+      
     }catch (error){
       console.log("error in signing up", error);
     }
@@ -43,13 +59,22 @@ const SignIn = ({onSignIn, onSignUp}) => {
         type="password"
         value={password}
         onChange={e=>setPassword(e.target.value)}
+      />      
+      </div>
+      <div>
+        <TextField
+        id="email"
+        label = "E-mail"
+        type="email"
+        value={email}
+        onChange={e=>setEmail(e.target.value)}
       />
       </div>
       <div>
       <Button id="signInButton" color="primary" onClick ={signIn}>
         로그인
       </Button>
-      <Button id="signUpButton" color="primary" onClick ={signIn}>
+      <Button id="signUpButton" color="primary" onClick ={signUp}>
         가입하기
       </Button>
       </div>
